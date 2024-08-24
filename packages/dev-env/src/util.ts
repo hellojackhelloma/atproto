16e76fb30b78f88c2fb838ece94ae872009e3042
hellojackhelloma <hellojack.helloma@gmail.com>
2024-08-24 10:59:32 +0800

    1

--- packages/dev-env/src/util.ts
+++ packages/dev-env/src/util.ts
@@ -26,7 +26,7 @@ export const mockResolvers = (idResolver: IdResolver, pds: TestPds) => {
     if (typeof service?.serviceEndpoint === 'string') {
       service.serviceEndpoint = service.serviceEndpoint.replace(
         pds.ctx.cfg.service.publicUrl,
-        'https://linxme.com',
+        'http://localhost:2583',
       )
     }
     return result
